b8b9b619ef62b91f39fca73ad6d3c3e126ae920f
niwatorinoko <rean.ogswr@gmail.com>
2024-04-01 10:59:51 +0800

    Created DetailView

--- diary/urls.py
+++ diary/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import DiaryListView, DiaryCreateView
+from .views import DiaryListView, DiaryCreateView, DiaryDetailView
 
 
 
 urlpatterns = [
-    path('diary/lists/', DiaryListView.as_view(), name='list'),
+    path('', DiaryListView.as_view(), name='list'),
+    path('diary/detail/<int:pk>', DiaryDetailView.as_view(), name='detail'),
     path('diary/create/', DiaryCreateView.as_view(), name='create'),
 ]
